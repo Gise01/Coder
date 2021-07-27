0a7dbf024edc0f165c8335795a72e34f6bb2f0c5
Gise01 <arenagise1@gmail.com>
2021-07-27 19:54:20 -0300

    Desafio 1 19900Arena

---
+++ 19900ArenaDesafio1/js/main.js
@@ -0,0 +1,5 @@
+let firstName = prompt("Cuál es tu nombre");
+let lastName = prompt("Cuál es tu apellido");
+let result = "Es un placer recibirte " + firstName + " " + lastName +"!!";
+
+document.write(result);

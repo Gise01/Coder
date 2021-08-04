ebf70f55996ff2829b3bb606a9fbc3a02ff229b5
Gise01 <arenagise1@gmail.com>
2021-08-04 20:17:51 -0300

    Primera modificacion

--- 19900ArenaProyecto/js/main.js
+++ 19900ArenaProyecto/js/main.js
@@ -1,12 +1,22 @@
-let height;
-let weight;
-let model;
+let height=0;
+let weight=0;
+let model=0;
 let confirmation;
 
 function start () {
-  height = parseFloat(prompt("Ingrese en mts el alto de su pared"));
-  weight = parseFloat(prompt("Ingrese en mts el ancho de su pared"));
-  model  = parseFloat(prompt("Indique que modelo de placa es la que necesita: 0.36, 0.1568, 0.3136"));
+  do{
+    height = parseFloat(prompt("Por favor ingrese un número valido en mts para el alto de su pared"));
+  } while (!height || height<0);
+ 
+  do{
+    weight = parseFloat(prompt("Por favor ingrese un número valido en mts para el ancho de su pared"));
+  } while (!weight || weight<0);
+
+  do {
+    model = parseFloat(prompt("Indique el modelo de placa que necesita segín las siguientes opciones: 0.36, 0.1568, 0.3136"));
+    console.log(model);
+  } while (!model)
+  
   confirmation = confirm(`Ud indico que la superficie a cubrir con placas de ${model}m2 tiene un alto de ${height}mts, un ancho de ${weight}mts, esto es correcto?`);
 }
 

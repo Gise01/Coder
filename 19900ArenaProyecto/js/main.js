02b0eaaced4fb6b46f1c3b9a559b988fc773bb14
Gise01 <arenagise1@gmail.com>
2021-08-05 18:32:50 -0300

    Primer parcial Proyecto

--- 19900ArenaProyecto/js/main.js
+++ 19900ArenaProyecto/js/main.js
@@ -12,25 +12,30 @@ function start () {
     weight = parseFloat(prompt("Por favor ingrese un número valido en mts para el ancho de su pared"));
   } while (!weight || weight<0);
 
-  do {
-    model = parseInt(prompt(`Indique el modelo de placa que necesita según las siguientes opciones:<br> 1) 0.36 + 2) 0.1568<br> + 3) 0.3136`));
-    console.log(model);
-  } while (!model);
+  function models () {
+    do {
+      model = parseInt(prompt(`Indique el modelo de placa que necesita según las siguientes opciones: 1) 0.36 \n2) 0.1568 \n3) 0.3136`));
+    } while (!model);
 
-  switch (model) {
-    case 1:
-      model = 0.36;
-      break;
-    case 2:
-      model = 0.1568;
-      break;
-    case 3:
-      model = 0.3136
-      break;
-    default:
-      model = parseInt(prompt(`Indique solo una de las opciones válidas:<br> 1) 0.36 + 2) 0.1568<br> + 3) 0.3136`))
+    switch (model) {
+      case 1:
+        model = 0.36;
+        break;
+      case 2:
+        model = 0.1568;
+        break;
+      case 3:
+        model = 0.3136
+        break;
+      default:
+        alert("Por favor debe solo indicar 1 o 2 o 3 según corresponda");
+      }
   }
-  
+
+  do { 
+    models();
+  } while (!model || model < 0 || model > 3);
+
   confirmation = confirm(`Ud indico que la superficie a cubrir con placas de ${model}m2 tiene un alto de ${height}mts, un ancho de ${weight}mts, esto es correcto?`);
 }
 
